e4d04931f6939dae3196046c139479c95637e5f1
DiegoRuiz05 <contatodiegosouzaruiz@gmail.com>
2025-09-16 15:54:52 -0300

    Primeiro commit

---
+++ AchePet/src/main/java/AchePetWebSite/AchePet/AchePetApplication.java
@@ -0,0 +1,13 @@
+package AchePetWebSite.AchePet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchePetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchePetApplication.class, args);
+	}
+
+}
---
+++ AchePet/src/test/java/AchePetWebSite/AchePet/AchePetApplicationTests.java
@@ -0,0 +1,13 @@
+package AchePetWebSite.AchePet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
